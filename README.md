69999a72189f02abe41fefc7fbc37c03c9775691
Anakha <anakhasatheeshpulickal1992@gmail.com>
2022-10-11 23:56:40 +0100

    LandingPageCompleted
    LandingPageCompleted
    
    LandingPageCompleted

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Landing Page Project
+# Anakha Satheesh - Landing Page Project
 
 ## Table of Contents
 
@@ -6,8 +6,6 @@
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+This is a landing page project which has a subscription form, section highlighting and smmoth scrolling.
+It includes 4 main sections.
+This is an interactive web page.
--- js/app.js
+++ js/app.js
@@ -40,39 +40,36 @@
 /*------------------All Sections-----------------*/ 
 const allSections = document.querySelectorAll('section');
 
-const navList=document.getElementById('navbar__list');
-let nav_but =document.createElement('li');
-
-
-// Add class 'active' to section when near top of viewport
-
 /* ------------------Navigation--------------------*/
+const navList = document.getElementById('navbar__list');
+let nav_button = document.createElement('li');
+
 navList.classList.add('navbar__menu');
-for (let i = 1; i <= sections.length; i++)
+for (let i = 1; i <= allSections.length; i++)
 {
     let nav_sec = document.createElement('li');
     nav_sec.classList.add('navbar__menu');
     let nav_link = document.createElement('a');
-    nav_link.classList.add('menu__link')
-    nav_link.setAttribute('href', '#section' + i);
-    nav_link.dataset.nav = 'Section ' + i;
+    nav_link.classList.add('menu__link');
+    nav_link.setAttribute('href','#section' + i)
+    nav_link.dataset.nav = 'Section '+ i;
     nav_sec.appendChild(nav_link);
-    navBarList.appendChild(nav_sec);
+    navList.appendChild(nav_sec);
     if ( i == 1)
     {
-        nav_link.textContent = '1';
+        nav_link.textContent = 'Section 1';
     }
     else if ( i == 2)
     {
-        nav_link.textContent = '2';
+        nav_link.textContent = 'Section 2';
     }
     else if (i == 3)
     {
-        nav_link.textContent = '3';
+        nav_link.textContent = 'Section 3';
     }
     else if (i == 4)
     {
-        nav_link.textContent = '4';
+        nav_link.textContent = 'Section 4';
     }
     else{
         nav_link.textContent = 'Section' + i;
@@ -85,23 +82,46 @@ felements.addEventListener('submit', function(event){
     confirm('Form Submitted Successfully');
 });
 
+/*------------Section Highlighting--------------*/ 
+window.addEventListener('scroll', sectionHighlight)
+function sectionHighlight(){
+    for (let i = 0; i< allSections.length; i++) {
+        const element = allSections[i];
+        const topLine = element.getBoundingClientRect().top;
+        const bottomLine = element.getBoundingClientRect().bottom;
+        const windowHt = window.innerHeight *0.5;
+        if(topLine < windowHt && bottomLine > windowHt){
+            return element.classList.add('your-active-class');
+        }
+        element.classList.remove('your-active-class')
+    }
+}
+/*---------=------All Anchors----------------*/ 
+const allAnchors = document.querySelectorAll('a');
+
+
+function jumpToSection (e)
+{
+    if (e.target.nodeName == 'A')
+    {
+        e.preventDefault();
+        console.log(e.target);
+        for (let section of allSections)
+        {
+            if (section.dataset.nav == e.target.dataset.nav)
+            {
+                section.scrollIntoView({behavior: "smooth", block: "start"});
+            }
+        }
+    }
+}
+navList.addEventListener('click', jumpToSection);
 
 
 
 
-// Scroll to anchor ID using scrollTO event
 
 
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
 
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
 
 
